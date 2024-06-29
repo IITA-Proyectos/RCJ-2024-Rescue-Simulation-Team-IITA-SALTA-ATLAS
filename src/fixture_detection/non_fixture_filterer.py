7f87bd1f77823e0adaaf100cc981d04be54031e2
nachososa02 <[nachososa2007@gmail.com]>
2024-06-29 11:45:51 -0300

    modify bgr values hole

--- src/fixture_detection/non_fixture_filterer.py
+++ src/fixture_detection/non_fixture_filterer.py
@@ -7,8 +7,8 @@ class NonFixtureFilter:
             ColorFilter((94, 112,  32), (95, 143, 139)), # walls
             ColorFilter((0, 0, 192,), (0, 0, 192)), # Normal Floor
             ColorFilter((0, 0, 29), (0, 0, 138)), # Obstacles
-            ColorFilter((0, 0, 10), (0, 0, 30)), # Outer Hole
-            ColorFilter((0, 0, 106), (0, 0, 106)), # Inner Hole
+            ColorFilter((0, 91, 7), (0, 110, 30)), # Outer Hole
+            ColorFilter((0, 93, 1), (0, 110, 6)), # Inner Hole
             ColorFilter((0, 205, 233), (0, 205, 233)), # Red Tile
             ColorFilter((107, 0, 72), (116, 90, 211)) # Checkpoint
 
